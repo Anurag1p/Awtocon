251339a476f61b9ca3c1efc42f92b18d9741a33a
Anurag Pal <anuragpal211@gmsil.com>
2024-09-24 18:44:06 +0530

    setup for docker

--- client/src/setProxy.js
+++ client/src/setProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const proxy = {
-    target: 'http://localhost:3001',
+    target: 'http://localhost:3009',
     changeOrigin: true
 }
 module.exports = function(app) {
